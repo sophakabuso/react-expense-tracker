81e96391d7865f18d6bcb320d4b7ccf1d77bb38a
sophakabuso <sopha.buso@gmail.com>
2023-07-04 16:01:21 +0200

    update fire

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
 import {useHistory,Link} from "react-router-dom"
+import { auth } from '../config/firebase';
 
 function Login() {
     
     const history =useHistory(); 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const goToHomePage = (()=>{
-        history.push('/home')
+
+        signInWithEmailAndPassword(auth, email, password).then(()=>{
+            alert("Login Success")
+            history.push('/home');
+        }).catch((error)=>{
+
+        }) 
 
     })
 
@@ -14,7 +25,11 @@ function Login() {
         <div>
             <h1>Login page</h1>
 
-            <button onClick={goToHomePage}>Go to home page</button><br></br><br></br>
+            <input type="text" placeholder='Enter Email' 
+              onChange={(e)=>setEmail(e.target)} />
+            <input type="password" placeholder='Enter Password'
+               onChange={(e)=>setPassword(e.target)} /><br></br><br></br>
+            <button onClick={goToHomePage}>Login</button><br></br><br></br>
             <Link to="signUp">Don't have an account:Sign</Link>
             
         </div>
