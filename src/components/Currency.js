8bb6d7a75caf9819aae8e1e679974bf64bf82727
sophakabuso <sopha.buso@gmail.com>
2023-07-05 16:04:54 +0200

    add

---
+++ src/components/Currency.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Currency (){
+    
+   return(
+       <div className="container">
+          <div>
+            <h4>Currency Converter</h4>
+            <div>
+
+            </div>
+            <input type="numseer" placeholder="Enter Amount"/>
+            
+          </div>
+        </div>
+
+   )
+}
+
+export default Currency;
